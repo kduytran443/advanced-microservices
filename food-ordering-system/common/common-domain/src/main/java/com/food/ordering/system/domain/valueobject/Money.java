2e7d08d06035e0920011f4f79f16f896197b8dd5
kduytran443 <trankhanhduy18@gmail.com>
2025-04-07 01:57:31 +0700

    Make common module

---
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
+    }
+}
